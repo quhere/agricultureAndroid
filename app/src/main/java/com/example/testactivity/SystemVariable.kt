fdf98d5083033bb9a52563f49096c7e7679f8e16
quhere <ngvuquang22@gmail.com>
2024-08-14 12:06:59 +0700

    fix display info

--- app/src/main/java/com/example/testactivity/SystemVariable.kt
+++ app/src/main/java/com/example/testactivity/SystemVariable.kt
@@ -2,6 +2,6 @@ package com.example.testactivity
 
 class SystemVariable {
     companion object {
-        var BASE_URL: String? = "http://172.31.99.85:8089/api/"
+        var BASE_URL: String? = "http://192.168.1.7:8089/api/"
     }
 }
